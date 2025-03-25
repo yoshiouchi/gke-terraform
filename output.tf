2468609b45abdd9d8905b5bc513cc241a64c4e84
Yoshi Ouchi <yoshi.ouchi@sysdig.com>
2025-03-25 14:23:31 +1100

    Made a correction in output.tf

--- output.tf
+++ output.tf
@@ -8,8 +8,7 @@ output "gke_cluster_endpoint" {
 }
 output "gke_cluster_master_version" {
   description = "The master Kubernetes version of the GKE cluster"
-  value       = google_container
-_cluster.primary.master_version
+  value       = google_container_cluster.primary.master_version
 }
 output "bastion_ip" {
   description = "The public IP of the bastion host"
