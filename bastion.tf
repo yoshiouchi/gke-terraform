279548099ba5c2dc500195c7b734f584a4614223
Yoshi Ouchi <yoshi.ouchi@sysdig.com>
2025-03-25 12:00:00 +1100

    Added service account roles in bastion.tf

--- bastion.tf
+++ bastion.tf
@@ -36,3 +36,15 @@ resource "google_compute_firewall" "allow_http_https_rdp" {
   source_ranges = ["0.0.0.0/0"]
   target_tags = ["allow-http-https-rdp"]
 }
+
+resource "google_service_account" "bastion_sa" {
+  account_id   = "bastion-sa"
+  display_name = "Bastion Service Account"
+}
+
+resource "google_project_iam_member" "bastion_sa_roles" {
+  for_each = toset(var.service_account_roles)
+  project = var.project_id
+  member  = "serviceAccount:${google_service_account.bastion_sa.email}"
+  role    = each.value
+}
